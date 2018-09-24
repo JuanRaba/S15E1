5c4701de697dfe296287026c5b35b42df3b88ff2
juanraba <jrabanillog@gmail.com>
2018-09-24 13:01:46 -0300

    session destroy, new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/sign_up', to: "users#new"
   post 'users', to: "users#create"
+  resources :sessions, only: [:new, :create, :destroy]
   root to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
