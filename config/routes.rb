0f67d56c5c35183e8b79718de4ee571a5ef9e0b5
juanraba <jrabanillog@gmail.com>
2018-09-24 12:25:41 -0300

    new y create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+  root to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
