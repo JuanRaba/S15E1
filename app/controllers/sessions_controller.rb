bfc71318a1398942fcb6b1c3fd6cf24ec0f73f2d
juanraba <jrabanillog@gmail.com>
2018-09-24 13:23:35 -0300

    session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
   def destroy
     reset_session
-    redirect_to root_path
+    redirect_to new_session_path
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user.nil?
+      redirect_to new_session_path, alert: 'el usuario no existe'
+      return
+    end
+  
+    if @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to new_session_path, alert: 'el password no es valido'
+    end
   end
 end
